55cf6c406409b7b41fb2106557610bf266926bab
CarolinaVanuchi <carolinavanuchi21@gmail.com>
2022-03-04 21:32:03 -0800

    layout da tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,56 @@ void main() {
 
 class BytebankApp extends StatelessWidget {
   const BytebankApp({Key? key}) : super(key: key);
+  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(
-        title: Text('Bytebank'),
-      ),)
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      home: Dashboard(),
     );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  static const primaryColor = Color.fromARGB(255, 0, 128, 128);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        title: const Text('Bytebank'),
+        backgroundColor: primaryColor
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/logo.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                height: 100,
+                width: 150,
+                color: primaryColor,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                  Icon(Icons.people, color: Colors.white, size: 32.0,),
+                  Text('Contact', style: TextStyle(color: Colors.white, fontSize: 18.0))
+                ]),
+            )
+            )
+          ],
+        ),
+      );
   }
 }
